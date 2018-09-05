8a1b41566bd9a64b6aa17d696c7407e61db2c0ce
sferguson670 <sferguson670@gmail.com>
2018-09-05 14:59:18 -0600

    Added comments for everyone's assignments

--- src/VPL.java
+++ src/VPL.java
@@ -166,7 +166,11 @@ public class VPL
  
       // implement all operations here:
       // ********************************************
-
+      // instruction 0 - 6: Sarah
+      // instructions 7 - 13: Eric
+      // instructions 14 - 20: Sara
+      // instructions 21 - 27: Ze
+      // instructions 28 - 34: Aimee
       // put your work right here!
 
       if ( op == oppCode ) {
