dd94738975072a4f53977c8c4ff1fe6410f1b291
eric-jacobson <ecjacobson9@gmail.com>
2018-11-02 22:13:25 -0600

    Fixed minor bug in Lexer and major debugging in Parser, seems to properly parse files now

--- Project2/Lexer.java
+++ Project2/Lexer.java
@@ -190,11 +190,8 @@ public class Lexer {
  
          if ( state == 2 ) {
             // see if data matches any special words
-            if ( data.equals("input")) {
+            if ( data.equals("nl") || data.equals("input")) {
                return new Token( "bif0", data );
-            }
-            else if ( data.equals("nl") ) {
-              return new Token( "bif1", "" );
             }
             else if ( data.equals("sqrt") || data.equals("cos") || 
                       data.equals("sin") || data.equals("atan") ||
